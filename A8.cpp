989a76c276f01431a2788c577fecc79f26a1c557
Waybhase Mangesh Popat <160419306+Mangesh2004@users.noreply.github.com>
2024-12-16 22:55:10 +0530

    Update A8.cpp

--- A8.cpp
+++ A8.cpp
@@ -1,47 +1,41 @@
-//  A palindrome is a string of character that's the same forward and backward. Typically, punctuation, capitalization, and spaces are ignored. For example, "Poor Dan is in a droop" is a palindrome, as can be seen
-// by examining the characters "poor danisina droop" and observing that they are the same forward and backward One way to check for a palindrome is to reverse the characters in the string and then compare with them
-// the original-in a palindrome, the sequence will be identical. Write C++ program with functions
-// a) To print original string followed by reversed string using stack
-// b) To check whether given string is palindrome or not
-
 #include <iostream>
 #include <cstring>
-#define MAX 50
 using namespace std;
+#define MAX 50
 
-class STACK
+class Stack
 {
 private:
     char a[MAX];
     int top;
 
 public:
-    STACK()
+    Stack()
     {
         top = -1;
     }
 
-    void push(char c)
+    void push(char x)
     {
         if (top >= MAX - 1)
         {
-            cout << "\nStack overflow, cannot push more characters.";
+            cout << "Stack overslow" << endl;
             return;
         }
-        top++;
-        a[top] = c;
-        a[top + 1] = '\0';
-        cout << c << " is pushed onto the stack...\n";
+        a[++top] = x;
+        cout << x << "is pushed into stack" << endl;
+    }
+
+    char pop(){
+        return a[top--];
     }
 
     void reverse() const
     {
-        cout << "\nReversed string is: ";
         for (int i = top; i >= 0; i--)
         {
             cout << a[i];
         }
-        cout << endl;
     }
 
     void convert(char str[])
@@ -50,54 +44,40 @@ public:
         for (int j = 0; str[j] != '\0'; j++)
         {
             if (isalnum(str[j]))
-            {                             // Check if character is alphanumeric
-                str[k] = tolower(str[j]); // Convert to lowercase
+            {
+                str[k] = tolower(str[j]);
                 k++;
             }
         }
         str[k] = '\0';
-        cout << "\nConverted String: " << str << endl;
-    }
-
-    void palindrome() const
+        cout << "comverted string is" << str << endl;
+    };
+    bool isPalindrome(char str[])
     {
-        char reversed[MAX];
-        int j = 0;
+       for(int i=0;str[i]!='\0';i++){
+            push(str[i]);
+       }
 
-        for (int i = top; i >= 0; i--, j++)
-        {
-            reversed[j] = a[i];
+       for(int i=0;str[i]!='\0';i++){
+        if(pop()!=str[i]){
+            return false;
         }
-        reversed[j] = '\0';
-
-        if (strcmp(reversed, a) == 0)
-        {
-            cout << "\nString is a palindrome...\n";
-        }
-        else
-        {
-            cout << "\nString is not a palindrome...\n";
-        }
+       }
+       return true;
     }
 };
 
 int main()
 {
-    STACK stack;
+    Stack s;
     char str[MAX];
-
-    cout << "Enter a string to check if it is a palindrome: ";
     cin.getline(str, MAX);
 
-    stack.convert(str);
-
-    for (int i = 0; str[i] != '\0'; i++)
-    {
-        stack.push(str[i]);
+    s.convert(str);
+    if(s.isPalindrome(str)){
+        cout<<"string is a plaiondrome"<<endl;
+    }else{
+        cout<<"String is not a pliondfrome";
     }
-
-    stack.palindrome();
-    stack.reverse();
-
     return 0;
 }
